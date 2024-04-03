24d5c079a6485c451856ab1d22c3f12cb055cf5c
lanseria <zhangchao564265135@hotmail.com>
2024-04-03 13:44:57 +0800

    add install hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,10 @@
 // eslint.config.js
+// @ts-check
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
   unocss: true,
-  formatters: {
-    css: true, // by default use Prettier
-    html: true, // by default use Prettier
-    toml: 'dprint', // use dprint for TOML
-    markdown: 'prettier', // use prettier for markdown
-  },
+  formatters: true,
   ignores: [
     'node_modules/',
     'public/',
